c728fc787cbe31f992936870dd8edc87746666f3
Ubuntu <ubuntu@ip-172-31-24-44.ap-southeast-2.compute.internal>
2013-07-10 12:16:35 +0000

    YAN web.js update

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html', function(err, data){
       if(err) throw err;
-      console.log(buf.toString(data));
+      console.log(data.toString());
   });
 });
 
